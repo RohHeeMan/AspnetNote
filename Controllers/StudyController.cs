b902158dc01c5dd78c45ffff2e0b950e7b34a009
노희만 <hmroh@hotmail.com>
2021-01-13 17:00:36 +0900

    StudyController 이용해서 간단한 페이지 작성 https://localhost:44360/study/csharp

---
+++ Controllers/StudyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetNote.Controllers
+{
+	public class StudyController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult	cSharp()
+		{
+			return View();
+		}
+
+		public IActionResult	Java()
+		{
+			return View(); 
+		}
+
+		public IActionResult	Cplus()
+		{
+			return View();
+		}
+	}
+}
